1164ddd78f41b329dea600ed7a130381a4b791bb
Syed Faraaz Ahmad <faraaz98@live.com>
2017-03-30 00:51:14 +0530

    fix description input height

---
+++ script/textarea.js
@@ -0,0 +1,9 @@
+$(function () {
+	var descriptionField = $('.description');
+	var titleField = $('.title');
+
+	var h = $(document).height() - titleField.height() - 40;
+
+	descriptionField.height(h);
+		
+});
